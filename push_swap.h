836ccf298ec088f1cc3ce1c87188fba7590b7a31
Andreas Maucher <andreas-maucher@gmx.de>
2023-04-06 14:28:09 +0200

    fixed library format

--- push_swap.h
+++ push_swap.h
@@ -36,71 +36,74 @@ struct	s_node
 };
 typedef struct s_node	t_node;
 
-/*sorter*/
-t_node	*three_sorter(t_node *h);
-t_node	*five_sorter_add_two(t_node *head);
-t_node	*five_sorter(t_node *head_a);
-
 /*utils*/
-size_t		ft_strlen(const char *s);
-int			lst_size(t_node *head);
-bool		check_if_sorted(t_node *head);
-void		freememory(t_node *head);
-long int	ft_atoi(const char *nptr);
-bool		ft_isdigit(int c);
-int			return_middle_value(t_node *head, int lsize);
-bool		loop_for_duplicates(long int *array, long int c, int ac);
-t_node		*creating_list_b(t_node *head_a);
+long int		ft_atoi(const char *nptr);
+size_t			ft_strlen(const char *s);
 
-/*target values*/
-t_sort_params	find_largest_value(t_node *head);
-int	find_smallest_number(t_node *head);
-t_sort_params	find_smallest_from_top(t_node *head, int ceiling);
-t_sort_params	find_smallest_from_bottom(t_node *head, int ceiling);
-int	find_third_highest_value(t_node *head);
+void			freememory(t_node *head);
+void			printlist(t_node *head);
+
+bool			ft_isdigit(int c);
+bool			loop_for_duplicates(long int *array, long int c, int ac);
+bool			check_if_sorted(t_node *head);
+
+int				return_middle_value(t_node *head, int lsize);
+int				lst_size(t_node *head);
+int				find_third_highest_value(t_node *head);
+int				find_smallest_number(t_node *head);
+/*calculations*/
+int				calculate_ratio(int length);
+int				calculate_ceiling(int ceiling, t_node *head_a);
+/*move counter*/
+int				rotate_counter(t_node *head, t_sort_params min_max);
+int				reverse_rotate_counter(t_node *head, t_sort_params target);
+int				find_smallest_number(t_node *head);
+int				find_third_highest_value(t_node *head);
+
+/*sorter*/
+t_node			*three_sorter(t_node *h);
+t_node			*five_sorter_add_two(t_node *head);
+t_node			*five_sorter(t_node *head_a);
+t_node			*creating_list_b(t_node *head_a);
 
 /*rules*/
-t_node	*swap_a(t_node *head);
-t_node	*swap_b(t_node *head);
-t_node	*rotate_a(t_node *head);
-t_node	*rotate_b(t_node *head);
+t_node			*swap_a(t_node *head);
+t_node			*swap_b(t_node *head);
+t_node			*rotate_a(t_node *head);
+t_node			*rotate_b(t_node *head);
 
 /*rules2*/
-t_node	*push_to_b(t_node *head_b, t_node *head_a);
-t_node	*push_to_a(t_node *head_a, t_node *head_b);
-t_node	*reverse_rotate_a(t_node *head);
-t_node	*reverse_rotate_b(t_node *head);
-
-/*move counter*/
-int	rotate_counter(t_node *head, t_sort_params min_max);
-int	reverse_rotate_counter(t_node *head, t_sort_params target);
+t_node			*push_to_b(t_node *head_b, t_node *head_a);
+t_node			*push_to_a(t_node *head_a, t_node *head_b);
+t_node			*reverse_rotate_a(t_node *head);
+t_node			*reverse_rotate_b(t_node *head);
 
 /*list operators*/
-t_node	*create_new_node(int value);
-t_node *new_list_insert_at_head(t_node **head, t_node *t_nodeo_insert);
-t_node	*insert_at_head(t_node *head, int new_value);
-t_node *delete_at_head(t_node *old_head);
-t_node *insert_at_tail(t_node *head, int new_value);
+t_node			*create_new_node(int value);
+t_node			*new_list_insert_at_head(t_node **head, t_node *t_nodeo_insert);
+t_node			*insert_at_head(t_node *head, int new_value);
+t_node			*delete_at_head(t_node *old_head);
+t_node			*insert_at_tail(t_node *head, int new_value);
 
 /*list operators2*/
-void	printlist(t_node *head);
-t_node *delete_at_tail(t_node *head);
-t_node *return_tail_value(t_node *head);
-t_node *delete_second_node(t_node *head);
-t_node	*create_new_list(t_node *head, int i, char **av);
-
-/*ft_split*/
-char	**ft_split(char const *s, char c);
-
-/*calculations*/
-int	calculate_ratio(int length);
-int	calculate_ceiling(int ceiling, t_node *head_a);
+t_node			*delete_at_tail(t_node *head);
+t_node			*return_tail_value(t_node *head);
+t_node			*delete_second_node(t_node *head);
+t_node			*create_new_list(t_node *head, int i, char **av);
 
 /*algorithm*/
-t_node	*ra_vs_rra(t_node *head, t_sort_params first, t_sort_params last);
-t_node	*find_shortest_path(t_node *head, int ceiling);
-t_node	*list_b_condition(t_node *head_b, t_node *head_a, int ceiling);
-t_node	*move_to_a(t_node *head_a, t_node *head_b);
-t_node	*insertion(t_node *head_a);
+t_node			*ra_vs_rra(t_node *head, t_sort_params first,
+					t_sort_params last);
+t_node			*find_shortest_path(t_node *head, int ceiling);
+t_node			*list_b_condition(t_node *head_b, t_node *head_a, int ceiling);
+t_node			*move_to_a(t_node *head_a, t_node *head_b);
+t_node			*insertion(t_node *head_a);
+
+/*ft_split*/
+char			**ft_split(char const *s, char c);
+
+t_sort_params	find_largest_value(t_node *head);
+t_sort_params	find_smallest_from_top(t_node *head, int ceiling);
+t_sort_params	find_smallest_from_bottom(t_node *head, int ceiling);
 
 #endif
