db4956267750a7ca095d9b1efef67e37440fc500
Andreas Maucher <andreas-maucher@gmx.de>
2023-04-26 11:27:09 +0200

    completely overhauled ratio calculation

--- calculations.c
+++ calculations.c
@@ -20,46 +20,11 @@ int	calculate_ratio(int n)
 	}
 	else if (n < 100)
 	{
-		return ((n - 10) * 0.06 + 1);
-	}
-	else if (n < 500)
-	{
-		return ((n - 100) * 0.02 + 14);
+		return (14);
 	}
 	else
-	{
-		return ((n - 500) * 0.006 + 30);
-	}
+		return (22);
 }
-
-/* below 100 always increasing by 0.6; division is getting smaller start with 1 and decrease each time by 0.1
-	20 1.6 12.5(20/1.6)
-	30 2.2 13.6
-	40 2.8 14.2
-	50 3.4 14,7
-	60 4	15
-	70 4.6  15.2
-	80 5.2  15.38
-	90 5.8  15.57
-
-	!!how much difference would it make if I just go with 14 the middle value
-	meaning ratio n < 100 = 14 does it really change that much?!!
-*/
-
-/* between 100 - 500 increase by 0.2; every 100 + 2
-110 14.2 7.7
-120 14.4 8.3
-130 14.6 8.9
-140
-150
-160
-170
-180
-190 15.8
-200 18
-300 20
-400 22
-*/
 
 int	calculate_ceiling(int ceiling, t_node *head_a)
 {
